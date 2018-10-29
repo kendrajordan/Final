57006eb87b9dcd1422d34d5be4ae0ea85f355650
Kendra <kendra.jordan8@gmail.com>
2018-10-28 23:49:57 -0400

    Started using laravel

---
+++ evblog/resources/views/electricdrift/Homepage.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Electric Drift</title>
+    <link href="/../css/app.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
+  </head>
+  <body class="bg-info container">
+      <nav class="navbar  navbar-primary bg-primary">
+        <div class="navbar-brand">Electric Drift</div>
+      </nav>
+      <div id="demo" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+      <ul class="carousel-indicators">
+        <li data-target="#demo" data-slide-to="0" class="active"></li>
+        <li data-target="#demo" data-slide-to="1"></li>
+        <li data-target="#demo" data-slide-to="2"></li>
+      </ul>
+
+      <!-- The slideshow -->
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="img-fluid"style="height:400px;width:100%"src="https://i1.wp.com/electrek.co/wp-content/uploads/sites/3/2017/01/electric-vehicle-charging-vs-gasoline-e1484590338347.jpg?resize=1600%2C792&quality=82&strip=all&ssl=1/800x400" alt="Los Angeles">
+        </div>
+        <div class="carousel-item">
+          <img class="img-fluid"style="height:400px;width:100%"src="https://c1cleantechnicacom-wpengine.netdna-ssl.com/files/2017/09/electric-car-fleet.jpg" alt="Chicago">
+        </div>
+        <div class="carousel-item">
+          <img class="img-fluid"style="height:400px;width:100%" src="https://s3.amazonaws.com/plugshare.production.photos/photos/351635.jpg" alt="New York">
+        </div>
+      </div>
+
+      <!-- Left and right controls -->
+      <a class="carousel-control-prev" href="#demo" data-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+      </a>
+      <a class="carousel-control-next" href="#demo" data-slide="next">
+        <span class="carousel-control-next-icon"></span>
+      </a>
+    </div>
+    <div class=" newev">
+      <h2 class="text-center">New to Evs?</h2>
+      <div class="row">
+          <div class="col-sm-4 d-flex flex-column align-items-center pr-0 pl-0 text">
+            <a href="#"><i class="far fa-question-circle fa-7x"></i><p class="text-center">F.A.Q</p></a>
+          </div>
+          <div class="col-sm-4 d-flex flex-column align-items-center pr-0 pl-0 text">
+            <a href="#"><i class="fas fa-charging-station fa-7x"></i><p class="text-center">Fuel comparison</p></a>
+          </div>
+          <div class="col-sm-4 d-flex flex-column align-items-center pr-0 pl-0 text">
+            <a href="#"><i class="fas fa-car fa-7x"></i><p class="text-center">Why Drive Electric</p></a>
+          </div>
+        </div>
+        <div class="currentev">
+          <h2 class="text-center">Already Own An EV?</h2>
+          <div class="row">
+            <div class="col-sm-6 d-flex flex-column align-items-center pr-0 pl-0 text">
+              <a href="#"><i class="fas fa-map-marked-alt fa-7x"></i><p class="text-center">Trip Planner</p></a>
+            </div>
+            <div class="col-sm-6 d-flex flex-column align-items-center pr-0 pl-0 text">
+              <a href="#"><i class="fas fa-table fa-7x"></i><p class="text-center">Charge Log</p></a>
+            </div>
+          </div>
+        </div>
+        <footer class="bg-primary">
+          <p>@2018</p>
+        </footer>
+    </div>
+
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ evblog/resources/views/navigation.blade.php
@@ -0,0 +1,20 @@
+<ul class="nav">
+  <li class="nav-item">
+    <a class="nav-link" href="/electricdrift">Electric drift Index</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="/electricdrift/Homepage">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="/electricdrift/faq">F.A.Q</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="/electricdrift/fuel comparison">Fuel Comparison</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="/electricdrift/Why drive">Why Drive Electric</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="/electricdrift/tripplanner">Trip Planner</a>
+  </li>
+</ul>
